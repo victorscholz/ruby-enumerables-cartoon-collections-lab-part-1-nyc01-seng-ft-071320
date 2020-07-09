1c0a29438455392a200e0d30421054d7bbeb5e3f
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 02:56:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,5 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  hash = Hash.new
-  %w(Dopey Grumpy Bashful).each_with_index { |item, index|
-    hash[item] = index
-  }
-  hash
+  
 end
