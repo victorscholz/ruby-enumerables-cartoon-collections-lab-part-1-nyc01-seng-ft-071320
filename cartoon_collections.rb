62b74e576f653266bd2e1adf6deee457cfb13657
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 02:50:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |characters|
+    puts "Hello #{characters}!"
+  end
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  hash = Hash.new
+  %w(Dopey Grumpy Bashful).each_with_index { |item, index|
+    hash[item] = index
+  }
+  hash
 end
